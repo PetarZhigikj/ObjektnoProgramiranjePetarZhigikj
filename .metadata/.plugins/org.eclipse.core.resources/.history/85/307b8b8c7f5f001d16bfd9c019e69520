30e426f7ee02efe735776272a542aa882e6195dd
unknown <zigicpetar12@gmail.com>
2022-11-08 18:34:44 +0100

    Domasna rabota chas 2

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/85/307b8b8c7f5f001d16bfd9c019e69520
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Petar
+ *
+ */
+module Zadaca1 {
+}
---
+++ Zadaca3/src/zadaca3/Covek.java
@@ -0,0 +1,12 @@
+package zadaca3;
+
+public class Covek {
+	public String ime;
+	public String prezime;
+	public String mBroj;
+	
+	//Default Constructor
+	public Covek () {
+		
+	}
+}
